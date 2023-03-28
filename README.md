18efed3b2aba7d5907616d628e2279bd35c709ba
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-03-27 20:53:40 -0600

    added GET,POST,PUT,DELETE to apiRoutes and imported Sequelize

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # E-commerce_Database
-Object-Relational Mapping (ORM): E-Commerce Back End
+
+Object-Relational Mapping (ORM): E-Commerce Back End 
+Mysql2, Express.js, 
+
+## Description
+
+build the back end for an e-commerce site by modifying starter code to configure a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## Fixes/Build Configuration
+
+API Routes to perform RESTful CRUD operations, seed the database, and sync Sequelize to the database on server start.
+## Deployment
+
---
+++ models/index.js
@@ -0,0 +1,40 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'clearance'
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Limited'
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
